2569a09ea5b6f6b5fe93b2f0fdd6348fc175513b
Raktim Raihan <raktim.raihan@bjitgroup.com>
2025-03-06 18:30:24 +0900

    Add userEmailAddress and userId to CreateTagDto
    Updated the CreateTagDto to include `userEmailAddress` with an email validation and `userId` as required fields. These additions enhance the DTO for better user association and data consistency.

--- src/tag/dto/create-tag.dto.ts
+++ src/tag/dto/create-tag.dto.ts
@@ -1,16 +1,20 @@
 import { OmitType } from '@nestjs/mapped-types';
 import { Tag } from '../entity/tag.entity';
-import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 import { User } from '../../user/entity/user.entity';
 import { ToDo } from '../../todo/entity/todo.entity';
 
-export class CreateTagDto extends OmitType(
-  Tag,
-  ['id', 'creationDate', 'updteDate'] as const,
-){
+export class CreateTagDto {
   @IsString()
   @Length(255)
   @IsNotEmpty()
   tagName: string;
+  
+  @IsNotEmpty()
+  @IsEmail()
+  userEmailAddress: string;
+
+  @IsNotEmpty()
+  userId: string;
 
 }
